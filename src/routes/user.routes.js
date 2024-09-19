2014abc6abbccfbcf32cd98e735e0a245cd3f574
RyanPeyrot <ryan.peyrot@gmail.com>
2024-09-19 20:26:14 +0200

    ajout de la gestion baniere et profil pour les company

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,6 +8,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: async (req, file) => {
+        console.log('File info:', file);
         const userId = req.params.id;  // Récupérer l'ID de l'utilisateur depuis les paramètres de la requête
         return {
             folder: 'profile_pictures',  // Dossier dans lequel les images sont stockées dans Cloudinary
@@ -17,7 +18,27 @@ const storage = new CloudinaryStorage({
     },
 });
 
-const upload = multer({ storage });
+const companyStorage = new CloudinaryStorage({
+
+    cloudinary: cloudinary,
+    params: async (req, file) => {
+        console.log('File info:', file);
+        const userId = req.params.id;
+        const folder = `company/${file.fieldname}`;
+        return {
+            folder: folder,  // Dossier spécifique pour chaque type d'image
+            public_id: `${userId}_${file.fieldname}`,
+            allowed_formats: ['jpg', 'png', 'jpeg'],
+        };
+    },
+
+});
+
+const upload = multer({storage: storage});
+const uploadCompany = multer({storage: companyStorage}).fields([
+    {name: 'profile_pic', maxCount: 1},  // Autoriser un seul fichier pour la photo de profil
+    {name: 'banner_pic', maxCount: 1},  // Autoriser un seul fichier pour la bannière
+]);
 
 router.get('/', controller.getAll);
 router.get('/artisanByRate', controller.getAllArtisansByRating);
@@ -31,6 +52,7 @@ router.post('/', controller.createOne);
 router.post('/login', controller.loginUser);
 router.post('/addCompany', controller.addCompany);
 router.post('/:id/profile_pic', upload.single('profile_pic'), controller.updateProfilePic);
+router.post('/:id/company_pic', uploadCompany, controller.updateCompanyPic);
 router.put('/:id', controller.updateOne);
 router.delete('/:id', controller.deleteOne);
 
