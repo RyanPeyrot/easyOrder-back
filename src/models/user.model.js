79944a6a794f6c9cfa9b5f0969a8d3fd28b8a2c7
RyanPeyrot <ryan.peyrot@gmail.com>
2024-09-21 20:04:39 +0200

    ajout du rôle admin

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -21,7 +21,7 @@ const userSchema = new Schema({
     },
     role:{
         type:String,
-        enum: ['artisan', 'client'],
+        enum: ['artisan', 'client', 'admin'],
         default: 'client'
     },
     company: companySchema,
